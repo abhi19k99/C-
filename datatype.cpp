90801a23f4c96c4178c36b2e68d291b6a418c720
abhi19k99 <157949165+abhi19k99@users.noreply.github.com>
2024-01-30 08:33:13 +0530

    hello & datatype

---
+++ datatype.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // integer datatype
+    int integerValueGiven=20;
+    cout << "Stored integer value is  :  "<< integerValueGiven<<endl;
+    
+    //Floating point datatype
+    float floatValue = 3.14;
+    cout<<"Stored float value is : "<< floatValue<<endl;
+
+    // character datatype
+    char charValue = 'A';
+    cout <<"Stored char value is : "<< charValue<<endl;
+
+    //Boolean datatype false
+    // bool boolValue = false;
+    // cout <<"Stored boolean value is : "<< boolValue<<endl;
+    
+    // Boolean datatype true
+     bool boolValue = true;
+    cout <<"Stored boolean value is : "<< boolValue<<endl;
+    
+     
+       
+
+    return 0;
+}
---
+++ hello.cpp
@@ -0,0 +1,35 @@
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     cout<<"Hello AD"<<endl;
+//     return 0;
+
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     string name;
+//     cout << "Enter your name: - ";
+//     cin >> name;
+//     cout << "Hello!" << name << endl;
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int a;
+//     cout << "How old are you? : - ";
+//     cin >> a;
+//     cout << "Your age is : - " <<a;
+//     return 0;
+// }
+#include <iostream>
+using namespace std;
+int main(){
+    int integerValueGiven=20;
+    cout << "Stored integer value is  : - "<< integerValue;
+    
+    return 0;
+}
