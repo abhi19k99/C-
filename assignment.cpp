bd2e77bdf887510a3534dcf29ef40ac6049e0f48
abhi19k99 <157949165+abhi19k99@users.noreply.github.com>
2024-01-30 08:25:13 +0530

    Assignment1 of c++

---
+++ assignment.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+string name;
+cout << "Enter your name:  ";
+cin >> name;
+
+
+int a;
+     cout << "How old are you? :  ";
+     cin >> a;
+     int b;
+     cout << "What is your weight? :  ";
+     cin >> b;
+     
+
+     char gender;
+     cout << "What is your gender? :  ";
+    cin >> gender;
+
+    cout << "Hello!" << name << endl;
+    cout << "Your age is :  " << a <<endl;
+    cout << "Your weight is :  " << b <<endl;
+    cout <<"Gender is : "<< gender<<endl;
+
+
+
+
+    return 0;
+}
